f7b353aca0602797d833c32fcaf4e85a36277c66
Jeff Hicks <jeffreyhicks90@gmail.com>
2020-10-19 14:11:43 -0700

    Created index for postgress router.

--- database-pg/dbHelpers.js
+++ database-pg/dbHelpers.js
@@ -4,21 +4,21 @@ const pool = require('./index.js');
 module.exports = {
 
   searchLocations: (searchTerm, callback) => {
-    pool.query(`SELECT city FROM locations WHERE city LIKE '${searchTerm}%' LIMIT 5;`, (err, results) => {
+    pool.query(`SELECT * FROM locations WHERE city ILIKE '${searchTerm}%' LIMIT 5;`, (err, results) => {
       if (err) {
         callback(err);
       } else {
-        callback(null, results);
+        callback(null, results.rows);
       }
     });
   },
 
   searchCamps: (searchTerm, callback) => {
-    pool.query(`SELECT name FROM locations WHERE name LIKE '${searchTerm}%' LIMIT 8;`, (err, results) => {
+    pool.query(`SELECT * FROM camps WHERE name ILIKE '${searchTerm}%' LIMIT 8;`, (err, results) => {
       if (err) {
         callback(err);
       } else {
-        callback(null, results);
+        callback(null, results.rows);
       }
     });
   },
